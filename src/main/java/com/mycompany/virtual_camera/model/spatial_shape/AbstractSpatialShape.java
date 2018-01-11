73a866ce415be5100d20f4b46e9be3565a128e3c
Pawel Mac <Pawel.Mac@gmail.com>
2018-01-12 00:54:03 +0100

    Updated class AbstractSpatialShape.

--- src/main/java/com/mycompany/virtual_camera/model/spatial_shape/AbstractSpatialShape.java
+++ src/main/java/com/mycompany/virtual_camera/model/spatial_shape/AbstractSpatialShape.java
@@ -1,8 +1,11 @@
 package com.mycompany.virtual_camera.model.spatial_shape;
 
 import com.mycompany.virtual_camera.model.Edge3D;
+import com.mycompany.virtual_camera.model.Face3D;
 import com.mycompany.virtual_camera.model.Point3D;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -13,6 +16,7 @@ public abstract class AbstractSpatialShape {
     
     protected final Set<Point3D> point3DsSet;
     protected final Set<Edge3D> edge3DsSet;
+    protected final List<Face3D> face3DsList = new ArrayList<>();
     
     public AbstractSpatialShape() {
         this.point3DsSet = new HashSet<>();
@@ -25,5 +29,9 @@ public abstract class AbstractSpatialShape {
     
     public Set<Edge3D> getEdge3DsSet() {
         return edge3DsSet;
+    }
+    
+    public List<Face3D> getFace3DsList() {
+        return face3DsList;
     }
 }
